e58615c5fa2891d53fadb6ce5e24e4b0fc0b9aa6
Benjamin Sherman <bcanfieldsherman@gmail.com>
2021-06-22 18:14:07 -0700

    Added name and one more dropout setting to sweep

--- test.py
+++ test.py
@@ -12,23 +12,24 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", type=str, required=True)
     parser.add_argument("--dataset", type=str, default="test-data:latest")
+    parser.add_argument("--name", type=str, default=None)
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     config = parse_args()
-    run = wandb.init(config=config, job_type="testing")
+    run = wandb.init(name=config.name, config=config, job_type="testing")
     dataset = run.use_artifact(config.dataset)
     dataset.download(root="data/")
     labels, fnames, audio, spectrograms = utils.load_data("data/test.npz")
     labels = utils.one_hot_labels(labels)
     model = run.use_artifact(config.model)
-    run.log(model.metadata)
     model_path = model.download(root="models/")
     model_name = config.model.split(":")[0]
     model = load_model(f"models/{model_name}.keras")
     model.summary()
-    model.evaluate(spectrograms, labels, callbacks=[WandbCallback()])
+    loss, acc = model.evaluate(spectrograms, labels)
+    run.log({"loss": loss, "accuracy": acc})
     preds = model.predict(spectrograms)
     run.log({"Confusion_Matrix": utils.make_confusion_matrix(preds, labels)})
     run.log(
